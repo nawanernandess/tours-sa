eeaa83ef726acf983a136005df3e445f138e7fec
Nawan Ernandes <nawanernandess@gmail.com>
2024-09-30 15:15:37 -0300

    ajustes de layout e pastas
    -- Remove arquivos que não estão sendo usados;
    -- Remove classes css que  não estão em uso;
    -- Melhora o componente de conteúdo.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,32 +7,29 @@ import { HttpClientModule } from '@angular/common/http';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
 import { ModalModule } from 'ngx-bootstrap/modal';
-import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FormsModule } from '@angular/forms';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatCardModule } from '@angular/material/card'
+import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSortModule } from '@angular/material/sort';
 
-
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { FooterComponent } from './layout/footer/footer.component';
-import { HomeComponent } from './site/home/home.component'
+import { HomeComponent } from './site/home/home.component';
 import { CadastroGuiaComponent } from './site/cadastro-guia-component/cadastro-guia.component';
 
 import { ContentComponent } from './site/content/content.component';
 import { ListaGuiaComponent } from './site/lista-guia-component/lista-guia.component';
 import { UpdateRegistrationModalComponent } from './site/lista-guia-component/update-registration-modal/update-registration-modal.component';
 import { DropdownComponent } from './layout/navbar/dropdown/dropdown.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
-
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [		
+  declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
@@ -41,8 +38,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
     ContentComponent,
     ListaGuiaComponent,
     UpdateRegistrationModalComponent,
-    DropdownComponent
-   ],
+    DropdownComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -60,11 +57,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
     MatButtonModule,
     MatIconModule,
     MatSortModule,
-    NgbModule
+    NgbModule,
   ],
-  providers: [
-    {provide : MAT_DIALOG_DATA, useValue : {}}
-  ],
-  bootstrap: [AppComponent]
+  providers: [{ provide: MAT_DIALOG_DATA, useValue: {} }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
